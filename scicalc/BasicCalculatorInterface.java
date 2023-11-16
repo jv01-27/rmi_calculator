915501afbcfcf98023b045d669718d24e15f3ea2
Josué Valentín Vázquez Inés <132970520+jv01-27@users.noreply.github.com>
2023-11-16 09:51:06 -0600

    Delete scicalc directory

--- scicalc/BasicCalculatorInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface BasicCalculatorInterface extends Remote {
-    double add(double x, double y) throws RemoteException;
-    double subtract(double x, double y) throws RemoteException;
-    double multiply(double x, double y) throws RemoteException;
-    double divide(double x, double y) throws RemoteException;
-}
